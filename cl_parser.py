c7fd5a72ce604c980213c252b63e514aa8170fd6
Malcev Nikita <maltsew.na@gmail.com>
2022-12-21 13:57:57 +0300

    Модуль для работы с парсером командной строки
    Каждый аргумент функционирует как отдельный аргумент парсера

---
+++ cl_parser.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 20 17:07:19 2022
+
+@author: n.maltsev
+"""
+
+
+import argparse
+
+def parse_command_line():
+    parser = argparse.ArgumentParser(description='Телефонная книга')
+    parser.add_argument(
+        '-a',
+        '--add_user',
+        nargs='?',
+        const='',
+        type=str,
+        default=None,
+        help='Добавляет новый контакт в телефонную книгу. \
+              Формат ввода данных <--Фамилия Имя Отчество \
+              Адрес электронной почты Телефон-->\
+              '
+    )
+    parser.add_argument(
+        '-ls',
+        '--show_contacts',
+        type=str,
+        default=None,
+        help='Показывает все контакты телефонной книги'
+    )
+    my_namespace = parser.parse_args()
+    return my_namespace
