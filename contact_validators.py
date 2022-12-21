5d55f0c3409f44021c4663b7d5fddcd1d4d40261
Malcev Nikita <maltsew.na@gmail.com>
2022-12-21 18:58:40 +0300

    Валидаторы для проверки имени, адреса почты и телефона
    Валидатор телефона временно заменил на более простой для более удобного
    тестирования

---
+++ contact_validators.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 21 17:21:07 2022
+
+@author: n.maltsev
+"""
+
+
+import re
+
+
+def contact_name_is_cyrrilic(text):
+    return bool(re.search('[а-яА-Я]', text))
+
+def contact_email_is_valid(email):
+    return bool(re.fullmatch(
+        '([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+',email))
+
+def contact_phone_is_valid(phone):
+    #return bool(re.fullmatch("8(?:-\\d{3}){2}(?:-\\d{2}){2}", phone))
+    return bool(re.fullmatch("8", phone))
