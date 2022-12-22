24e0bc59a5abf76a887b2ffdcb2f115814047f3f
Malcev Nikita <maltsew.na@gmail.com>
2022-12-22 16:13:04 +0300

    Правки в модуле менеджера контактов:
    1. Добавил функцию для обработки введенных данных
    2. Переписал логику функции валидации данных контакта
    3. Дополнил список TO-DO по модулю

--- contacts_manager.py
+++ contacts_manager.py
@@ -7,28 +7,80 @@ Created on Tue Dec 20 18:16:21 2022
 
 
 from cl_parser import parse_command_line
+from DB_worker import add_contact_to_table
+from contact_validators import contact_name_is_cyrrilic, contact_email_is_valid, contact_phone_is_valid
 
-def add_user():
-        f = parse_command_line()
-        if f.add_user is not None:
-            if f.add_user != '':
-                new_user = f.add_user.split(" ")
-                print(new_user)
-                if len(new_user) == 3:
-                    print('<--Добавлен новый контакт-->')
-                    print('ФИО', new_user[0], '|',
-                          'Адрес электронной почты', new_user[1], '|',
-                          'Номер телефона', new_user[2], '|')
+
+
+def validate_contact():
+    try:
+        print('Вызов в валидации')
+        new_contact_name, new_contact_email, new_contact_phone = contact_info_insertion()
+        if contact_name_is_cyrrilic(new_contact_name):
+            if contact_email_is_valid(new_contact_email):
+                if contact_phone_is_valid(new_contact_phone):
+                    return True
                 else:
-                    print('Ошибка! Неправильный формат ввода')
+                    print('Номер телефона введен неверно')
             else:
-                print('Новый контакт не может быть пустым. Пожалуйста, \
-                      дополните информацию о контакте \
-                          или обратитесь за справкой указав команду -h')
-        return 1
+                print('Адрес почты введен неверно')
+        else:
+            print('Имя введено неверно')
+    except TypeError:
+        print('!!!')
+    return False
+
+
+# def validate_contact():
+#     f = parse_command_line()
+#     if f.add_user == '':
+#         print('!!!')
+#         try:
+#             new_contact_name, new_contact_email, new_contact_phone = contact_info_insertion()
+#             if contact_name_is_cyrrilic(new_contact_name):
+#                 if contact_email_is_valid(new_contact_email):
+#                     if contact_phone_is_valid(new_contact_phone):
+#                         return True
+#                     else:
+#                         print('Номер телефона введен неверно')
+#                 else:
+#                     print('Адрес почты введен неверно')
+#             else:
+#                 print('Имя введено неверно')
+#         except TypeError:
+#             print('!!!')
+#     else:
+#        print('Ошибка! Неправильный формат ввода')
+#        print('Пожалуйста, дополните информацию о контакте \
+#                             или обратитесь за справкой указав команду -h')
+#     return False
+
+    
+def add_contact():
+    if validate_contact():
+        print('вызов в добавлении')
+        contact_name, contact_email, contact_phone = data
+        add_contact_to_table(contact_name=contact_name,
+                             contact_email=contact_email,
+                             contact_phone=contact_phone)
+    
     
 def show_contacts():
         f = parse_command_line()
         all_contacts = f.show_contacts
         print(all_contacts)
         return 1
+
+
+def contact_info_insertion():
+    print('Введите ФИО')
+    input_name = input()
+    print('Введите адрес электронной почты')
+    input_email = input()
+    print('Введите номер телефона')
+    input_phone = input()
+    global data
+    data = (input_name, input_email, input_phone)
+    return input_name, input_email, input_phone
+
+    
