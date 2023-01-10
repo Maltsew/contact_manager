36e3e1296dc7313e9939f10945fb10b824f8acd4
Malcev Nikita <maltsew.na@gmail.com>
2023-01-10 16:31:14 +0300

    Убрал захардкоженное имя БД
    Для реализации функционала перелючения БД в менеджере контактов ввел
    переменную с именем подключаемой БД, которая передается из функции
    db_selector() модуля работы с БД

--- DB_worker.py
+++ DB_worker.py
@@ -8,10 +8,15 @@ Created on Tue Dec 20 15:22:19 2022
 
 import sqlite3
 import pandas as pd
+from DB_logic import selected_db
 
+
+
+global selected_db
+#database_name = selected_db
 
 def create_table_user():
-    conn = sqlite3.connect('database')
+    conn = sqlite3.connect(selected_db)
     c = conn.cursor()
     c.execute(
     '''
@@ -21,7 +26,7 @@ def create_table_user():
     conn.commit()
     
 def select_all_contacts():
-    conn = sqlite3.connect('database')
+    conn = sqlite3.connect(selected_db)
     c = conn.cursor()
     c.execute(
     '''
@@ -31,11 +36,14 @@ def select_all_contacts():
     if len(df) != 0:
         df.columns = ['ФИО', 'Адрес эл. почты', 'Телефон']
         print(df)
+        return True
+    else:
+        return False
 
     
     
 def add_contact_to_table(contact_name, contact_email, contact_phone):
-    conn = sqlite3.connect('database')
+    conn = sqlite3.connect(selected_db)
     c = conn.cursor()
     c.execute("INSERT INTO users (user_name, user_email, user_phone)\
               VALUES (?, ?, ?)",
@@ -43,7 +51,7 @@ def add_contact_to_table(contact_name, contact_email, contact_phone):
     conn.commit()
     
 def drop_user_table():
-    conn = sqlite3.connect('database')
+    conn = sqlite3.connect(selected_db)
     c = conn.cursor()
     c.execute(
     '''
@@ -55,7 +63,7 @@ def contact_search_query(contact_info):
     ''' Выполняет запрос на поиск информации о контакте в соответствии с заданным
     паттерном contact_info. Поиск происходит по всем полям БД users (с целью
     ускороить поиск)'''
-    conn = sqlite3.connect('database')
+    conn = sqlite3.connect(selected_db)
     c = conn.cursor()
     pattern = '%' + str(contact_info) + '%'
     c.execute("SELECT user_name, user_email, user_phone FROM users \
@@ -66,15 +74,17 @@ def contact_search_query(contact_info):
     if len(df) != 0:
         df.columns = ['ФИО', 'Адрес эл. почты', 'Телефон']
         print(df)
+        return True
     else:
         print('           Поиск', contact_info, 'не вернул результата!')
+        return False
         
 def delete_contact_query(contact_name, conctact_email):  
     
     ''' Выполняет запрос на удаление контактов в БД в соответствии с заданным
     паттерном contact_info'''
     
-    conn = sqlite3.connect('database')
+    conn = sqlite3.connect(selected_db)
     c = conn.cursor()
     c.execute("DELETE FROM users WHERE user_name = ? AND user_email = ?",
               (contact_name, conctact_email)
@@ -86,7 +96,7 @@ def contact_verification_query(contact_name, conctact_email):
     ''' Запрос для проверки наличия в БД контакта с данными, в точности
     соответствующим введенным пользователем'''
     
-    conn = sqlite3.connect('database')
+    conn = sqlite3.connect(selected_db)
     c = conn.cursor()
     c.execute("SELECT user_name, user_email FROM users \
               WHERE user_name = ? AND user_email = ?",
@@ -97,8 +107,8 @@ def contact_verification_query(contact_name, conctact_email):
     return False
     
     
-def select_all_contacts_for_export(file_name):
-    conn = sqlite3.connect('database')
+def select_all_contacts_for_export(file_name='default'):
+    conn = sqlite3.connect(selected_db)
     c = conn.cursor()
     c.execute(
     '''
@@ -106,6 +116,11 @@ def select_all_contacts_for_export(file_name):
     ''')
     df = pd.DataFrame(c.fetchall())
     if len(df) != 0:
-        excel_file_name = str(file_name) + ".xlsx"
-        df.to_excel(excel_file_name, sheet_name="Contacts")
+        try:
+            excel_file_name = str(file_name) + ".xlsx"
+            df.to_excel(excel_file_name, sheet_name="Contacts")
+        except ValueError:
+            print('Имя не может быть пустым')
+            return False
+            
             
