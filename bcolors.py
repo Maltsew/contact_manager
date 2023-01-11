34e130a388dab90185ae915f3707ad51f2f48a4e
Malcev Nikita <maltsew.na@gmail.com>
2023-01-11 15:22:48 +0300

    Класс управляющей последовательности ANSI для задания цвета и разновидности шрифта в выводе print()

---
+++ bcolors.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 10 17:05:15 2023
+
+@author: n.maltsev
+"""
+
+
+class bcolors:
+    HEADER = '\033[95m' #1
+    OKBLUE = '\033[94m' #2
+    OKCYAN = '\033[96m' #3
+    OKGREEN = '\033[92m' #4
+    WARNING = '\033[93m' #5 желтый
+    FAIL = '\033[91m' #6 красный
+    ENDC = '\033[0m' #7
+    BOLD = '\033[1m' #8
+    UNDERLINE = '\033[4m' #9
